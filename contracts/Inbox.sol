9d31d87142f28130fcb6a59e69278c8999627688
David Kremenitzer <dkrem21@gmail.com>
2022-02-08 11:40:04 -0500

    compiled node code

--- contracts/Inbox.sol
+++ contracts/Inbox.sol
@@ -1,4 +1,15 @@
 pragma solidity ^0.4.17;
 // linter warnings (red underline) about pragma version can igonored!
 
-// contract code will go here
+contract Inbox { // contract named Inbox
+    string public message; // storage variable (instance variable) exist for life of contract
+
+    function Inbox(string initialMessage) public { // The contructor if function has SAME syntax as contract name
+        message = initialMessage;
+    }
+
+    function setMessage(string newMessage) public { //funcs that we can call, ON the contract...
+        message = newMessage;                      // after it's been deployed to the blockchain
+    }
+
+}
